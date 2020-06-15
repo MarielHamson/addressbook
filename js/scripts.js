edc51360105d86f17f9f2ef789cc8c59aee3d242
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-14 21:23:44 -0700

    add address book and logic to push contact to address book

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
+//business logic for Address Book
+function AddressBook() {
+  this.contacts = [];
+}
+
+AddressBook.prototype.addContact = function (contact) {
+this.contacts.push(contact);
+}
+
+// business logic for Contacts 
+
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName;
   this.lastName = lastName;
